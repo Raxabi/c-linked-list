09bfb35b8eee0cbc1b081d0c05464025b88641ef
Raxabi <92801687+Raxabi@users.noreply.github.com>
2026-03-29 03:59:14 +0200

    feat: added idx field to `Node` struct

--- go/ll/node.go
+++ go/ll/node.go
@@ -1,12 +1,14 @@
 package ll
 
 type Node[T any] struct {
+	idx  int
 	Data T
 	Next *Node[T]
 }
 
-func N_New[T any](data T) Node[T] {
+func N_New[T any](idx int, data T) Node[T] {
 	return Node[T]{
+		idx:  idx,
 		Data: data,
 		Next: nil,
 	}
